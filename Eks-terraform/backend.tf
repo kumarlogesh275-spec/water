b123dd1e82d675bf010a797f59ec9a7533f34560
kumarlogesh275-spec <kumarlogesh275@gmail.com>
2025-12-26 13:29:27 +0530

    third

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "myjk123"  # ace your actual S3 bucket name
+    bucket = "home1230"  # ace your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "eu-west-2"
   }
